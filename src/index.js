81c9a1bcf59bac99c40e70174ffbd08aac8c2749
truong nguyen <44205504+truongginjs@users.noreply.github.com>
2019-10-23 22:20:45 +0700

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { Provider } from "react-redux";
 // import thunk from 'redux-thunk'
 import './index.css';
-import App from './app'
+import App from './App'
 import * as serviceWorker from './serviceWorker';
 import store from "./store/index";
 
